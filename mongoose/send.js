17cc409ea3e5368763aa31c5ef4f7662e3a026ab
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-03-04 20:44:17 +0530

    Add files via upload

--- mongoose/send.js
+++ mongoose/send.js
@@ -1,4 +1,12 @@
-{
-'fine'='one';
+ const mongoose = require('mongoose')
 
-}
+
+ const schema = new mongoose.Schema({
+ record : { type:String, required:true},
+ date : { type:Number, required:true},
+ })
+
+const put = mongoose.model('schema', schema)
+
+ module.exports.schema=put;
+

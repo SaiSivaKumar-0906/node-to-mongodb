e7a7b004447bb5e61c10e6444e18f373041d39c4
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-03-04 20:44:50 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const mongoose = require('mongoose');
-const bodyParser = require("body-parser");
+const schema = require('./mongoose/send')
+
+console.log(schema)
 
 mongoose.connect('mongodb://localhost/to-do-list')
 
+app.use(express.json())
+
 app.use("/front", express.static(path.resolve(__dirname, "front")));
 
-
-app.use(bodyParser.json());
 
 app.post("/api/create", async (req, res) => {
   const record = req.body;
@@ -20,6 +22,6 @@ app.post("/api/create", async (req, res) => {
   res.json({ status: "ok" });
 });
 
-app.listen(1900, () => {
-  console.log("Server up");
+app.listen(9669, () => {
+  console.log("http://localhost:"+9669);
 });

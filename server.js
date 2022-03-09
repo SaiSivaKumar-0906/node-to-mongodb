a359ae7aea81065962121a8aefc906336e4ad86a
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-03-09 12:13:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 const mongoose = require('mongoose');
-const schema = require('./mongoose/send')
+const models = require('./mongoose/models')
 
-console.log(schema)
 
 mongoose.connect('mongodb://localhost/to-do-list')
 
 app.use(express.json())
 
-app.use("/front", express.static(path.resolve(__dirname, "front")));
-
-
 app.post("/api/create", async (req, res) => {
   const record = req.body;
   console.log(record);
-
-  console.log(response);
-
-  res.json({ status: "ok" });
+  let two = await models.create(record)
+  console.log(two)
 });
 
 app.listen(9669, () => {

8fd72fb846bf6ad5115a62c6ee9c1d81e25401f0
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-03-09 12:13:09 +0530

    Update send.js

--- mongoose/send.js
+++ mongoose/send.js
@@ -1,12 +1,11 @@
- const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
 
  const schema = new mongoose.Schema({
  record : { type:String, required:true},
- date : { type:Number, required:true},
+ date : { type:Number, default:96}
  })
 
 const put = mongoose.model('schema', schema)
 
- module.exports.schema=put;
-
+ module.exports=put;

3b25939fb301a8b72a049c52312d7e62b03d11a5
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-03-12 21:58:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use(express.json())
 app.post("/api/create", async (req, res) => {
   const record = req.body;
   console.log(record);
-  let two = await models.create(record)
+  let two = models.create(record)
   console.log(two)
 });
 
 app.listen(9669, () => {
   console.log("http://localhost:"+9669);
-});
+}); 

75a46dc4387dac4670fb7969c721a7d45c87923a
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-03-23 19:09:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,26 @@ mongoose.connect('mongodb://localhost:27017/FOR-CRUD')
 
 app.use(express.json())
 
+
+app.get("/api/get", (req, res)=>{
+  const record = [{four : "finger"}, {five : "three"}]
+  res.json(record)
+})
+
 app.post("/api/create", async (req, res) => {
   const record = req.body;
   console.log(record)
   const one = await models.create({record})
   console.log(one)
 });
+
+app.get("/api/read", async (req, res)=>{
+  const response = await models.findById({one})
+  console.log(response)
+  res.json(response)
+})
+
+
 
 app.listen(9669, () => {
   console.log("http://localhost:"+9669+'/api/create');
